17d8e25a6ec5626f0d8abde8e4d512fe223764aa
Bram Borggreve <borggreve@gmail.com>
2022-01-05 19:15:54 -0500

    feat: implement Keypair page

--- libs/web/keypair/feature/src/lib/web-keypair-feature.tsx
+++ libs/web/keypair/feature/src/lib/web-keypair-feature.tsx
@@ -1,11 +1,34 @@
+import { WebUiButton } from '@kin-laboratory/web/ui/button';
+import { WebUiCard } from '@kin-laboratory/web/ui/card';
 import { WebUiPage } from '@kin-laboratory/web/ui/page';
+import { Keypair, SimpleKeypair } from '@kin-sdk/client';
+import { useState } from 'react';
 
 export interface WebKeypairFeatureProps {}
 
 export function WebKeypairFeature(props: WebKeypairFeatureProps) {
+  const [keypair, setKeypair] = useState<SimpleKeypair>();
+
+  function generate() {
+    const kp = Keypair.randomKeys();
+    setKeypair(kp);
+  }
+
   return (
     <WebUiPage>
-      <h1>WebKeypairFeature!</h1>
+      <WebUiCard>
+        <div className="flex flex-col space-y-6">
+          <div>
+            <WebUiButton onClick={generate} label="Generate" />
+          </div>
+          {keypair ? (
+            <div>
+              <div>SecretKey: {keypair.secret}</div>
+              <div>PublicKey: {keypair.publicKey}</div>
+            </div>
+          ) : undefined}
+        </div>
+      </WebUiCard>
     </WebUiPage>
   );
 }
