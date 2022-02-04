d08122d512d3ed00d59e55b368cf5dfe6d8063d6
Richard Mands <richard@devwell.org>
2022-02-04 18:35:25 +0000

    Airdrop in Keypair Tab

--- libs/web/airdrop/feature/src/lib/web-airdrop-feature.tsx
+++ libs/web/airdrop/feature/src/lib/web-airdrop-feature.tsx
@@ -1,116 +1,90 @@
 import { WebUiButton } from '@kin-laboratory/web/ui/button';
 import { WebUiCard } from '@kin-laboratory/web/ui/card';
 import { WebUiPage } from '@kin-laboratory/web/ui/page';
-import {
-  KinClient,
-  KinEnvironment,
-  KinTest,
-  SimpleKeypair,
-} from '@kin-sdk/client';
+import { SimpleKeypair } from '@kin-sdk/client';
 import { KinAccountBalance } from '@kin-sdk/client/src/lib/agora/kin-agora-client';
 import { useState, useEffect } from 'react';
 
+import { airdrop, createAccount, getBalances, openExplorer } from '../helpers';
+
 export interface WebAirdropFeatureProps {}
 
-const kin = new KinClient(KinTest);
-
-export function WebAirdropFeature(props: WebAirdropFeatureProps) {
-  const [amount, setAmount] = useState<string>('50000');
+export function AirdropCard({
+  fixedPublicKey,
+  sessionStorageKeypairs,
+}: {
+  fixedPublicKey?: string;
+  sessionStorageKeypairs: SimpleKeypair[];
+}) {
+  const [amount, setAmount] = useState<string>('');
   const [balances, setBalances] = useState<KinAccountBalance[]>();
   const [balanceNull, setBalanceNull] = useState(false);
-  const [publicKey, setPublicKey] = useState<string>();
+  const [publicKey, setPublicKey] = useState<string>(fixedPublicKey || '');
   const [dropping, setDropping] = useState<boolean>(false);
   const [error, setError] = useState<string>('');
-  const [sessionStorageKeypairs, setSessionStorageKeypairs] = useState<
-    SimpleKeypair[] | null
-  >(null);
 
   useEffect(() => {
-    const kps = sessionStorage.getItem('keyPairs') || '';
+    setBalances([])
+  }, [fixedPublicKey])
 
-    if (kps.length) {
-      setSessionStorageKeypairs(JSON.parse(kps));
-    }
-  }, []);
-
-  async function airdrop() {
-    if (publicKey) {
-      setDropping(true);
-      setError('')
-
-      try {
-        const [_, err] = await kin.requestAirdrop(publicKey, amount);
-        console.log("🚀 ~ err", err)
-
-        if (err === 'NOT_FOUND') {
-          const keyPair =
-            sessionStorageKeypairs?.length &&
-            sessionStorageKeypairs.find(
-              (kp: SimpleKeypair) => kp.publicKey === publicKey
-            );
-
-          if (keyPair && keyPair.secret) {
-            await kin.createAccount(keyPair.secret);
-            await airdrop();
-          } else {
-            setError("Sorry, we couldn't find your keypair. Try again using a keypair generated on the 'Keypair' tab.")
-          }
-        }
-
-        await getBalances();
-      } catch (err) {
-        console.error(`An error occurred`, err);
+  const airdropButton = (
+    <WebUiButton
+      onClick={() =>
+        publicKey &&
+        airdrop({
+          publicKey,
+          setDropping,
+          setError,
+          amount,
+          sessionStorageKeypairs,
+          setBalances,
+          setBalanceNull,
+        })
       }
-      setDropping(false);
-    }
-  }
+      label="Airdrop"
+      disabled={!publicKey || !amount || dropping}
+    />
+  );
 
-  async function getBalances() {
-    setBalanceNull(false);
-    try {
-      const [res, err] = await kin.getBalances(publicKey!);
+  const createAccountButton = (
+    <WebUiButton
+      disabled={!publicKey || dropping}
+      onClick={() =>
+        publicKey && createAccount({ publicKey,
+          setDropping,
+          setError,
+          amount,
+          sessionStorageKeypairs,
+          setBalances,
+          setBalanceNull,})
+      }
+      label="Create Account"
+    />
+  )
 
-      if (res === null) setBalanceNull(true);
+  const balancesButton = (
+    <WebUiButton
+      disabled={!publicKey}
+      onClick={() =>
+        publicKey && getBalances({ publicKey, setBalances, setBalanceNull })
+      }
+      label="Get Balances"
+    />
+  );
 
-      setBalances(res);
-    } catch (error) {
-      console.error(`An error occurred`, error);
-    }
-  }
+  const seeAccountButton = (
+    <WebUiButton
+      disabled={!publicKey}
+      onClick={() => publicKey && openExplorer({ publicKey })}
+      label="See Account in Explorer"
+    />
+  );
 
-   function getExplorerUrl(env: KinEnvironment, publicKey: string): string {
-    const baseUrl = `https://explorer.solana.com/address/${publicKey}`;
-    const validator = `https://local.validator.agorainfra.dev`;
-    const params =
-      env === KinEnvironment.Test ? `?cluster=custom&customUrl=${validator}` : '';
 
-    const finalUrl = `${baseUrl}/tokens${params}`;
-
-    return finalUrl;
-  }
-
-  function openExplorer() {
-    window.open(getExplorerUrl(KinEnvironment.Test, publicKey!), '_blank');
-  }
 
   return (
-    <WebUiPage>
-      <h1>
-        <span className="mt-2 block text-3xl text-left leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-          Airdrop Some Kin
-        </span>
-      </h1>
-      <div className="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
-        <p>
-          Once you have some Kin in your account, you can use it for testing
-          your Application. This only works on the Kin Test Net.
-        </p>
-      </div>
-
-      <br />
-
-      <WebUiCard>
-        <div className="flex flex-col space-y-6">
+      <div className="flex flex-col space-y-4">
+        {!fixedPublicKey ? (
           <div>
             <label
               htmlFor="publicKey"
@@ -130,60 +104,85 @@ export function WebAirdropFeature(props: WebAirdropFeatureProps) {
               />
             </div>
           </div>
-          <div>
-            <label
-              htmlFor="amount"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Amount
-            </label>
-            <div className="mt-1">
-              <input
-                value={amount}
-                onChange={(e) => setAmount(e.target?.value?.toString())}
-                type="number"
-                min={1}
-                max={50000}
-                name="amount"
-                id="amount"
-                className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-                placeholder="50000"
-              />
-            </div>
-          </div>
-          <div className="flex space-x-2">
-            <WebUiButton
-              onClick={airdrop}
-              label="Airdrop"
-              disabled={!publicKey || dropping}
-            />
+        ) : null}
 
-            <WebUiButton
-              disabled={!publicKey}
-              onClick={getBalances}
-              label="Get Balances"
-            />
+        {!fixedPublicKey ? null : (<div className="flex space-x-2">
+          {createAccountButton}
+          {airdropButton}
+          {balancesButton}
+          {seeAccountButton}
+        </div>)}
 
-            {/* TODO turn this back on when we've moved to Solana Dev net or link works to custom url */}
-            <WebUiButton
-              disabled={!publicKey}
-              onClick={openExplorer}
-              label="See Account in Explorer"
+        <div>
+          <label
+            htmlFor="amount"
+            className="block text-sm font-medium text-gray-700"
+          >
+            {fixedPublicKey ? 'Airdrop amount' : 'Amount'}
+          </label>
+          <div className="mt-1">
+            <input
+              value={amount}
+              onChange={(e) => setAmount(e.target?.value?.toString())}
+              type="number"
+              min={1}
+              max={50000}
+              name="amount"
+              id="amount"
+              className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+              placeholder="50000"
             />
-          </div>
-          <div className="">
-            {balanceNull === true && error
-              ? error
-              : null}
-            {balanceNull === true && !error
-              ? `Can't find account. Sending an Airdrop will create your account if it doesn't already exist.`
-              : null}
-            {!balanceNull && balances ? (
-              <pre>{JSON.stringify(balances, null, 2)}</pre>
-            ) : null}
           </div>
         </div>
-      </WebUiCard>
+
+        {fixedPublicKey ? null : (<div className="flex space-x-2">
+          {airdropButton}
+          {balancesButton}
+          {seeAccountButton}
+        </div>)}
+        {balanceNull === true && error ? error : null}
+          {balanceNull === true && !error
+            ? `Can't find account. Sending an Airdrop will create your account if it doesn't already exist.`
+            : null}
+          {!balanceNull && balances?.length ? (
+            <pre>{JSON.stringify(balances, null, 2)}</pre>
+          ) : null}
+      </div>
+  );
+}
+
+export function WebAirdropFeature(props: WebAirdropFeatureProps) {
+  const [sessionStorageKeypairs, setSessionStorageKeypairs] = useState<
+    SimpleKeypair[] | []
+  >([]);
+
+  useEffect(() => {
+    const kps = sessionStorage.getItem('keyPairs') || '';
+
+    if (kps.length) {
+      setSessionStorageKeypairs(JSON.parse(kps));
+    }
+  }, []);
+
+  return (
+    <WebUiPage>
+      <h1>
+        <span className="mt-2 block text-3xl text-left leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+          Airdrop Some Kin
+        </span>
+      </h1>
+      <div className="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
+        <p>
+          Once you have some Kin in your account, you can use it for testing
+          your Application. This only works on the Kin Test Net.
+        </p>
+      </div>
+
+      <br />
+          <WebUiCard>
+
+      <AirdropCard sessionStorageKeypairs={sessionStorageKeypairs} />     </WebUiCard>
+
     </WebUiPage>
   );
 }
