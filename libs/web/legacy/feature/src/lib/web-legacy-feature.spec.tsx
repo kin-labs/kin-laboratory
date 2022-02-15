98012d5ca3afda33e9dfea170f982ab53909f13e
Richard Mands <richard@devwell.org>
2022-02-15 16:51:52 +0000

    Added Legacy Tab and Keypair

---
+++ libs/web/legacy/feature/src/lib/web-legacy-feature.spec.tsx
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react';
+
+import WebLegacyFeature from './web-legacy-feature';
+
+describe('WebLegacyFeature', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(<WebLegacyFeature />);
+    expect(baseElement).toBeTruthy();
+  });
+});
