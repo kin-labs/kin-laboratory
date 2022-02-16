4e37d67a36425e169fbb879e6f7965a3fd3a7d5e
Bram Borggreve <borggreve@gmail.com>
2022-02-15 22:09:52 -0500

    fix: improve pre-formatted text

--- libs/web/ui/pre/src/lib/web-ui-pre.tsx
+++ libs/web/ui/pre/src/lib/web-ui-pre.tsx
@@ -4,7 +4,10 @@ export interface WebUiPreProps {}
 
 export function WebUiPre(props: PropsWithChildren<WebUiPreProps>) {
   return (
-    <pre className="p-4 text-sm bg-gray-50 rounded rounded-md border border-gray-200">
+    <pre
+      className="p-4 text-sm bg-gray-50 rounded rounded-md border border-gray-200 "
+      style={{ whiteSpace: 'pre-wrap', wordWrap: 'break-word' }}
+    >
       {props.children}
     </pre>
   );
