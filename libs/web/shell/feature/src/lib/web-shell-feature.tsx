c2215b712ed23b1d7c81bbde523f6efe648121db
Bram Borggreve <borggreve@gmail.com>
2022-07-01 19:32:25 +0100

    feat: move Legacy keypair converter to Keypair page

--- libs/web/shell/feature/src/lib/web-shell-feature.tsx
+++ libs/web/shell/feature/src/lib/web-shell-feature.tsx
@@ -2,7 +2,6 @@ import { WebAirdropFeature } from '@kin-laboratory/web/airdrop/feature';
 import { WebCalculatorFeature } from '@kin-laboratory/web/calculator/feature';
 import { WebHomeFeature } from '@kin-laboratory/web/home/feature';
 import { WebKeypairFeature } from '@kin-laboratory/web/keypair/feature';
-import { WebLegacyFeature } from '@kin-laboratory/web/legacy/feature';
 import { WebMemoFeature } from '@kin-laboratory/web/memo/feature';
 import { WebTransactionFeature } from '@kin-laboratory/web/transaction/feature';
 import { WebUiLayout, WebUiLinks } from '@kin-laboratory/web/ui/layout';
@@ -14,10 +13,9 @@ const links: WebUiLinks = [
   { label: 'Home', path: '/home' },
   { label: 'Keypair', path: '/keypair' },
   { label: 'Airdrop', path: '/airdrop' },
-  { label: 'Fee calculator', path: 'calculator' },
   { label: 'Memo', path: '/memo' },
   { label: 'Transaction', path: '/transaction' },
-  { label: 'Legacy', path: '/legacy' },
+  { label: 'Fee calculator', path: 'calculator' },
 ];
 
 export function WebShellFeature() {
@@ -29,7 +27,7 @@ export function WebShellFeature() {
         <Route path="/calculator" element={<WebCalculatorFeature />} />
         <Route path="/home" element={<WebHomeFeature />} />
         <Route path="/keypair" element={<WebKeypairFeature />} />
-        <Route path="/legacy" element={<WebLegacyFeature />} />
+        <Route path="/legacy" element={<Navigate to="/keypair" />} />
         <Route path="/memo" element={<WebMemoFeature />} />
         <Route path="/transaction" element={<WebTransactionFeature />} />
       </Routes>
